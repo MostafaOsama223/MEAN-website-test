e246835e10473449f1007990d6d6b30f64937043
MostafaOsama223 <mostafaosama03@hotmail.com>
2020-07-28 07:48:47 +0200

    Finished the group features (get all, rmv, crt and show one)

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var data = require('../db/data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
+});
+
+router.get('/grp/:grpName', function(req, res){
+  var group = {};
+  data.groups.forEach((val, ind) => {
+    if(val.grpName.replace(/\s/g,'').toLowerCase() === req.params.grpName){
+      group = val;
+      return;
+    }
+  });
+  res.send(group);
 });
 
 module.exports = router;
--- routes/teacher.js
+++ routes/teacher.js
@@ -1,20 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
-var users = [
-    {'user':'123@mmm.com','pass':'1234'},
-    {'user':'mostafa@mmm.com','pass':'2222'}
-];
+var data = require('../db/data.json');
 
 router.post('/log', function (req, res) {
     var sent = false;
-    users.forEach(user => {
+    data.users.forEach(user => {
         if(user.user === req.body.user && user.pass === req.body.pass){
             sent = true;
             res.send('ok');
         }    
     });
     if(!sent)    res.send('no');
+});
+
+router.get('/getTests', function(req, res){
+    res.send(data.tests);
+});
+
+router.get('/getGroups', function(req, res){
+    res.send(data.groups);
+});
+
+router.get('/getStudents', function(req, res){
+    res.send(data.students);
+});
+
+router.post('/rmv/grp', function(req, res){
+    var newGroups = [];
+    data.groups.forEach((grp) => {
+        if(grp.name != req.body.grp)    newGroups.push(grp);
+    });
+    data.groups = newGroups;
+    res.send(data.groups);
+});
+
+router.post('/crt/grp', function(req, res){
+    data.groups.push(req.body);
+    res.send('ok');
 });
 
 module.exports = router;
